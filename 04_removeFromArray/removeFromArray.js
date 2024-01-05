146162a2f5e819613530f3d364507c9424b4cbef
terencewang101 <149046885+terencewang101@users.noreply.github.com>
2024-01-06 03:36:41 +0800

    Pass first test of fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, deleted) {
+    
+        const newArray = array.filter(function (item) {
+            return item !== deleted;
+        })
+        return newArray;
 
 };
 
