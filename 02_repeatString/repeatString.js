29f3ffb1669f486cfc3f0c44c9bdaae2cc667da9
terencewang101 <149046885+terencewang101@users.noreply.github.com>
2024-01-06 02:35:14 +0800

    Finish exercises 2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let mergedString = "";
+    if (num < 0) return "ERROR";
+    else for (i = 0; i < num; i++) {
+        mergedString += string;
+    }
+    return mergedString;
 };
 
 // Do not edit below this line
