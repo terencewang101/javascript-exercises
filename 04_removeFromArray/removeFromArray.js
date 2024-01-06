194fe717a968ce4f0cd10af6f00372e6cc28387f
terencewang101 <149046885+terencewang101@users.noreply.github.com>
2024-01-06 17:48:04 +0800

    Wrote function that works but assumes all arguments are arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,15 @@
 const removeFromArray = function(array, deleted) {
-    
-        const newArray = array.filter(function (item) {
-            return item !== deleted;
-        })
-        return newArray;
-
+    //run through each item in the deleted array
+    for (i = 0; i < deleted.length; i++) {
+        // for each deleted item, run through each item in array and check for matching items
+        // identify the array index of said matching item
+        const index = array.indexOf(deleted[i]);
+        // splice it out of array
+        if (~index) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
