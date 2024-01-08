329d707d61d885d624b98311440a7e76bb672a07
terencewang101 <149046885+terencewang101@users.noreply.github.com>
2024-01-08 14:10:28 +0800

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,14 @@
 const sumAll = function(int1, int2) {
     // check which argument is larger value
-    if (int1 > int2) {
-        bigInt = int1; 
-        smallInt = int2;
-    } else {
-        bigInt = int2; 
-        smallInt = int1;
+    if (int1 < 0 || int2 < 0 || !Number.isInteger(int1) || !Number.isInteger(int2)) {
+        return "ERROR";
+    } else if (int1 > int2) {
+        [int1, int2] = [int2, int1];
     }
     // create new variable to hold sum
     let total = 0;
     // run a loop that increments from int1 to int2
-    for (i = smallInt; i <= bigInt; i++) {
+    for (i = int1; i <= int2; i++) {
         // for every integer less than int2, add into sumAll
         total += i;
     }
