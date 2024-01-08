3e231b4f39dea0576f97deb654322cebc8dfd953
terencewang101 <149046885+terencewang101@users.noreply.github.com>
2024-01-08 12:28:28 +0800

    Wrote function that passes first test of sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    // create new variable to hold sum
+    let total = 0;
+    // run a loop that increments from int1 to int2
+    for (i = int1; i <= int2; i++) {
+        // for every integer less than int2, add into sumAll
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
