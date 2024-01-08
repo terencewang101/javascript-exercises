f352b29e2efbb2d7a8be9cf32e9d81884d8f9df1
terencewang101 <149046885+terencewang101@users.noreply.github.com>
2024-01-08 14:43:58 +0800

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let celciusNew;
+  if (typeof fTemp == "number") {
+    celciusNew = (fTemp-32)*5/9;
+    celciusRounded = Math.round(celciusNew * 10) / 10;
+  }
+  return celciusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  if (typeof cTemp == "number") {
+    fahrenheitNew = cTemp * 9 / 5 + 32;
+    fahrenheitRounded = Math.round(fahrenheitNew * 10) / 10;
+  }
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
