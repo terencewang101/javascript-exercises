738c815afb98fe71fb0085f54551e02fa6af841d
terencewang101 <149046885+terencewang101@users.noreply.github.com>
2024-01-08 13:50:44 +0800

    Wrote function that passes first test of sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,16 @@
 const sumAll = function(int1, int2) {
+    // check which argument is larger value
+    if (int1 > int2) {
+        bigInt = int1; 
+        smallInt = int2;
+    } else {
+        bigInt = int2; 
+        smallInt = int1;
+    }
     // create new variable to hold sum
     let total = 0;
     // run a loop that increments from int1 to int2
-    for (i = int1; i <= int2; i++) {
+    for (i = smallInt; i <= bigInt; i++) {
         // for every integer less than int2, add into sumAll
         total += i;
     }
